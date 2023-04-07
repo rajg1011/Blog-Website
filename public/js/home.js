4c16e331b4b61db000759f1055efd2b6fc5fe2d2
Raj <raj2356.be21@chitkara.edu.in>
2023-04-07 19:46:19 +0530

    Changes in home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,17 @@
-const url= getCurrentURL();
-console.log(url);
-if(url.indexOf("/about")>-1){
-    document.getElementById("active-link").classList.remove("active");
+const url= window.location.href;
+const home= document.getElementById("home");
+const about=document.getElementById("about")
+console.log(url);   //It will print in windows console
+if(url.indexOf("about")>-1){
+    home.classList.remove("active");
+    about.classList.add("active");
 }
+else if(url.indexOf("contact")>-1){
+    home.classList.remove("active");
+    about.classList.remove("active");
+    document.getElementById("contact").classList.add("active");
+}
+else if(url.indexOf("compose")>=0) {
+    home.classList.remove("active");
+}
+
